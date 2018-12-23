fdf94b460fc719e8210111bae3ab68ddd69b60a5
Syfin <masse.wayne@gmail.com>
2018-12-23 06:39:19 -0500

    Initial commit

---
+++ desktop/src/org/laser/box2dtutorial/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.laser.box2dtutorial.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.laser.box2dtutorial.Box2DTutorial;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Box2DTutorial(), config);
+	}
+}
